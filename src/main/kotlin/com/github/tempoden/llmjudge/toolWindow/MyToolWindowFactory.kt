4e2fdc0da82adc4f15f238b3eb17504402668d1a
GitHub Action <action@github.com>
2025-05-18 06:21:43 +0000

    Template cleanup

--- src/main/kotlin/com/github/tempoden/llmjudge/toolWindow/MyToolWindowFactory.kt
+++ src/main/kotlin/com/github/tempoden/llmjudge/toolWindow/MyToolWindowFactory.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.toolWindow
+package com.github.tempoden.llmjudge.toolWindow
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.diagnostic.thisLogger
@@ -8,8 +8,8 @@ import com.intellij.openapi.wm.ToolWindowFactory
 import com.intellij.ui.components.JBLabel
 import com.intellij.ui.components.JBPanel
 import com.intellij.ui.content.ContentFactory
-import org.jetbrains.plugins.template.MyBundle
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.tempoden.llmjudge.MyBundle
+import com.github.tempoden.llmjudge.services.MyProjectService
 import javax.swing.JButton
 
 
